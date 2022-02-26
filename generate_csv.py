56b024a616d734a3bdfc3733f4e5a0f0c0aea5e4
Morten W. Petersen <morphex@gmail.com>
2022-02-26 21:03:05 +0100

    Adding ability to sort out transactions by year in CSV

--- generate_csv.py
+++ generate_csv.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
+import sys
+import datetime, time
 from database import initialize_transactions, initialize_blocks
 
 index, transactions = initialize_transactions()
@@ -9,15 +11,37 @@ main_account = ''
 receivers = []
 receivers_data = {}
 
+year = 1970
+
+try:
+    year = int(sys.argv[1])
+except IndexError:
+    pass
+
+if year == 1970:
+    start = datetime.date(year, 1, 1)
+    end = datetime.datetime.now()
+else:
+    start = datetime.date(year, 1, 1)
+    end = datetime.date(year + 1, 1, 1)
+
+#print(start.timetuple(), end.timetuple())
+
+start = time.mktime(start.timetuple())
+end = time.mktime(end.timetuple())
+#print(start, end)
+
 for block, transaction in transactions:
     if not main_account:
         main_account = transaction['to']
+    timestamp = blocks[block]['timestamp']
+    if timestamp < start or timestamp > end: continue
     to = transaction['to']
     if not to in receivers:
         receivers.append(to)
         receivers_data[to] = []
     receivers_data[to].append((transaction['from'], transaction['to'], transaction['value'],
-				transaction['gas'], transaction['gasPrice'], block, blocks[block]['timestamp']))
+				transaction['gas'], transaction['gasPrice'], block, timestamp))
 
 for receiver in receivers:
     file = open(receiver+'.csv', 'w')

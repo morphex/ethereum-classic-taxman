4d9f80dcff09f666f6af7106e08acb2abbfbbaca
Morten W. Petersen <morphex@gmail.com>
2022-03-06 21:57:54 +0100

    Making it clear what is being tested against.

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "Testing a randomly selected address with few and recent transactions"
+echo "Testing a randomly selected ETC address with few and recent transactions"
 
 ./run.py 0x222f266B603e4fa6D5605b5ec1F1C86E35C19F7D 14671157
 

b563edaf0d365b0a1d9b627b5136c4da86688998
Morten W. Petersen <morphex@gmail.com>
2022-03-06 11:52:39 +0100

    Making database handling safer

--- database.py
+++ database.py
@@ -2,7 +2,7 @@
 
 import time
 import json
-import sys
+import sys, os
 from connection import w3
 from utilities import dump_exception
 from web3._utils.encoding import Web3JsonEncoder as jsonencoder
@@ -11,11 +11,21 @@ transactions_database = "transactions.json"
 blocks_database = "blocks.json"
 rates_database = "rates.json"
 
+data_backups_dir = "./data_backups/"
+temporary_extension = ".tmp"
+old_extension = ".old"
+
 last_block_index = 0
 transactions = []
 
+try:
+    os.mkdir(data_backups_dir)
+except FileExistsError:
+    # print("Data backup directory already exists")
+    pass
+
 def _create_transactions_database():
-    with open(transactions_database, "w") as file:
+    with open(transactions_database, "x") as file:
         file.write(json.dumps((last_block_index, transactions), cls=jsonencoder))
 
 def initialize_transactions():
@@ -34,9 +44,10 @@ def initialize_transactions():
 
 def save_transactions(index, transactions):
     print("Saving transactions..")
-    # FIXME, rename before save
-    with open(transactions_database, "w") as file:
+    with open(transactions_database + temporary_extension, "w") as file:
         file.write(json.dumps((index, transactions), cls=jsonencoder))
+    os.rename(transactions_database, data_backups_dir + transactions_database + "." + str(time.time()) + old_extension)
+    os.rename(transactions_database + temporary_extension, transactions_database)
 
 #initialize_transactions()
 
@@ -45,7 +56,7 @@ blocks = {}
 
 def _create_blocks_database():
     global numbers, blocks
-    with open(blocks_database, "w") as file:
+    with open(blocks_database, "x") as file:
         file.write(json.dumps((numbers, blocks), cls=jsonencoder))
 
 def initialize_blocks():
@@ -66,8 +77,11 @@ def save_blocks(numbers, blocks):
     print("Saving blocks..")
     numbers.sort()
     # FIXME, rename before save
-    with open(blocks_database, "w") as file:
+    with open(blocks_database + temporary_extension, "w") as file:
         file.write(json.dumps((numbers, blocks), cls=jsonencoder))
+    os.rename(blocks_database, data_backups_dir + blocks_database + "." + str(time.time()) + old_extension)
+    os.rename(blocks_database + temporary_extension, blocks_database)
+
 
 #initialize_blocks()
 
@@ -75,7 +89,7 @@ rates = {}
 
 def _create_rates_database():
     global rates
-    with open(rates_database, "w") as file:
+    with open(rates_database, "x") as file:
         file.write(json.dumps(rates, cls=jsonencoder))
 
 def initialize_rates():
@@ -96,5 +110,7 @@ def initialize_rates():
 def save_rates(rates):
     print("Saving rates..")
     # FIXME, rename before save
-    with open(rates_database, "w") as file:
+    with open(rates_database + temporary_extension, "w") as file:
         file.write(json.dumps(rates, cls=jsonencoder))
+    os.rename(rates_database, data_backups_dir + rates_database + "." + str(time.time()) + old_extension)
+    os.rename(rates_database + temporary_extension, rates_database)

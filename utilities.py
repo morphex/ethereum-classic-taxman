ab1097299223bff63d7ac94d1a9913d0387d62ba
Morten W. Petersen <morphex@gmail.com>
2022-03-06 11:53:03 +0100

    Handling all errors in exception dump

--- utilities.py
+++ utilities.py
@@ -2,7 +2,10 @@ import traceback
 
 def dump_exception(exception):
     print("Exception:")
-    print(type(exception))
-    print((exception.args,))
-    print(exception)
-    traceback.print_last()
+    try:
+        print(type(exception))
+        print((exception.args,))
+        print(exception)
+        traceback.print_last()
+    except:
+        print("Exception during printing of exception")

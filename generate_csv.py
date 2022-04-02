ea4856654da70ec74e69147049b8ea3439bac83e
Morten W. Petersen <morphex@gmail.com>
2022-04-02 23:05:46 +0200

    Adding calculation of real gas cost

--- generate_csv.py
+++ generate_csv.py
@@ -8,6 +8,9 @@ index, transactions = initialize_transactions()
 numbers, blocks = initialize_blocks()
 rates = initialize_rates()
 
+from decimal import Decimal
+GWEI_DENOMINATOR = Decimal(1000000000.0)
+
 BLOCK_TIMESTAMP_TIMEZONE = pytz.timezone("UTC")
 
 main_account = ''
@@ -54,16 +57,22 @@ for block, transaction in transactions:
     timestamp_date = str(timestamp_datetime.date())
     timestamp_time = str(timestamp_datetime.time())
     try:
-        rate = rates[timestamp_date][0]
+        rate = Decimal(rates[timestamp_date][0])
+        gas_price_gwei = transaction['gasPrice']
+        gas_price_eth = ((Decimal(gas_price_gwei) / GWEI_DENOMINATOR) * 21000) / 1000000000
+        gas_price_usd = gas_price_eth * rate
     except KeyError:
         rate = -1
-    receivers_data[to].append((transaction['from'], transaction['to'], transaction['value'],
-				transaction['gas'], transaction['gasPrice'], block, timestamp,
-				timestamp_date, timestamp_time, rate))
+        gas_price_gwei = 0
+        gas_price_eth = -1
+        gas_price_usd = -1
+    receivers_data[to].append((transaction['from'], transaction['to'], transaction['hash'], transaction['value'],
+				gas_price_eth, gas_price_gwei, gas_price_usd, block,
+				timestamp, timestamp_date, timestamp_time, rate))
 
 for receiver in receivers:
     file = open(receiver+'.csv', 'w')
-    file.write(','.join(("From", "To", "Value", "Gas", "Gas price", "Block",
+    file.write(','.join(("From", "To", "Hash", "Value", "GAS Price ETH", "GAS Price Gwei", "Gas price USD", "Block",
 			"Timestamp", "Timestamp date", "Timestamp time", "Exchange rate")) + "\n")
     for transaction in receivers_data[receiver]:
         file.write(','.join(map(str, transaction)) + '\n')

5dfdb6dae432d82351b292093c53911dad4c7bc4
Morten W. Petersen <morphex@gmail.com>
2022-04-03 10:20:11 +0200

    Adding fields to generated CSV, re-organizing rate-related logic

--- generate_csv.py
+++ generate_csv.py
@@ -56,23 +56,24 @@ for block, transaction in transactions:
     timestamp_datetime = datetime.datetime.fromtimestamp(timestamp, tz=BLOCK_TIMESTAMP_TIMEZONE).astimezone(accounting_timezone)
     timestamp_date = str(timestamp_datetime.date())
     timestamp_time = str(timestamp_datetime.time())
+    value = (Decimal(transaction['value']) / GWEI_DENOMINATOR) / 1000000000
+    gas_price_gwei = transaction['gasPrice']
+    gas_price_eth = ((Decimal(gas_price_gwei) / GWEI_DENOMINATOR) * 21000) / 1000000000
     try:
         rate = Decimal(rates[timestamp_date][0])
-        gas_price_gwei = transaction['gasPrice']
-        gas_price_eth = ((Decimal(gas_price_gwei) / GWEI_DENOMINATOR) * 21000) / 1000000000
         gas_price_usd = gas_price_eth * rate
+        value_usd = value * rate
     except KeyError:
         rate = -1
-        gas_price_gwei = 0
-        gas_price_eth = -1
         gas_price_usd = -1
-    receivers_data[to].append((transaction['from'], transaction['to'], transaction['hash'], transaction['value'],
-				gas_price_eth, gas_price_gwei, gas_price_usd, block,
+        value_usd = -1
+    receivers_data[to].append((transaction['from'], transaction['to'], transaction['hash'], value,
+				value_usd, gas_price_eth, gas_price_usd, gas_price_gwei, block,
 				timestamp, timestamp_date, timestamp_time, rate))
 
 for receiver in receivers:
     file = open(receiver+'.csv', 'w')
-    file.write(','.join(("From", "To", "Hash", "Value", "GAS Price ETH", "GAS Price Gwei", "Gas price USD", "Block",
+    file.write(','.join(("From", "To", "Hash", "Value", "Value USD", "GAS Price ETH", "Gas price USD", "GAS Price Gwei", "Block",
 			"Timestamp", "Timestamp date", "Timestamp time", "Exchange rate")) + "\n")
     for transaction in receivers_data[receiver]:
         file.write(','.join(map(str, transaction)) + '\n')

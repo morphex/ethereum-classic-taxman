59b255edab4c2c0d68b2ffa4d0839698e8ed958f
Morten W. Petersen <morphex@gmail.com>
2022-04-24 23:46:18 +0200

    Adding a tools directory and a blocks reloader.

---
+++ tools/blocks_reload.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+import sys, os
+sys.path.append(os.getcwd())
+
+from connection import w3
+from database import initialize_transactions, initialize_blocks, save_blocks
+
+index, transactions = initialize_transactions()
+
+print(index, len(transactions))
+
+numbers, blocks = initialize_blocks()
+
+print(numbers, len(blocks))
+
+for block_number, transaction in transactions:
+    if not block_number in numbers:
+        numbers.append(block_number)
+    print("Saving block", block_number, numbers)
+    block = w3.eth.get_block(hex(block_number), full_transactions=True)
+    blocks[block_number] = block
+
+save_blocks(numbers, blocks)

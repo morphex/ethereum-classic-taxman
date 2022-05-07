d7c5467b6a8d17a186abaf10a73d618c32efbd28
Morten W. Petersen <morphex@gmail.com>
2022-05-07 19:30:52 +0200

    Updating test info

--- test.sh
+++ test.sh
@@ -5,5 +5,7 @@ echo "Testing a randomly selected ETC address with few and recent transactions"
 ./run.py 0x222f266B603e4fa6D5605b5ec1F1C86E35C19F7D 14671157
 #Disabled, because CSV file might be missing
 #./parse_exchange_rates.py
+# Norwegian central bank CSV file format
+#./parse_exchange_rates_fiat.py ../USD-NOK.csv \; ,
 ./generate_csv.py
 

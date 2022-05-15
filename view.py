a947f61e5389e74c1dfd728ad7067e6d0e0229cb
Morten W. Petersen <morphex@gmail.com>
2022-05-15 21:42:53 +0200

    Adding some info

--- view.py
+++ view.py
@@ -2,6 +2,17 @@
 
 import pprint, sys, json
 
+# Little tool for viewing JSON data / making it easier to edit.
+#
+# For example
+#
+# ./view.py transactions.json | less
+#
+# or
+#
+# ./view.py transactions.json > transactions_edit.json
+# nano transactions_edit.json
+
 pp = pprint.PrettyPrinter()
 
 try:
